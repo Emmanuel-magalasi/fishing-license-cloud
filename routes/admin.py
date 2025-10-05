d06787a846f28b940bf1ec84856be04012d9f6c8
Emmanuel <emmanuelmagalasi004@gmail.com>
2025-10-05 16:09:02 +0200

    Fix: import 'request' in admin.py to enable payment initiation after license approval

--- routes/admin.py
+++ routes/admin.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, flash, redirect, url_for, send_file, current_app
+from flask import Blueprint, render_template, flash, redirect, url_for, send_file, current_app, request
 from flask_login import login_required, current_user
 from datetime import datetime
 import qrcode
